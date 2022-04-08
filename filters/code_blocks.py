e82c48a63b9a5b076beb0dbdd647416f46231a2a
dcbu <dennis@dennismail.de>
2022-04-09 00:09:56 +0200

    (c) fixes

--- filters/code_blocks.py
+++ filters/code_blocks.py
@@ -4,7 +4,7 @@
 Processes code blocks and changes code to t-vim based solution
 """
 
-from panflute import run_filter, CodeBlock, RawBlock
+from panflute import run_filter, CodeBlock, RawBlock, Para
 import logging
 
 def action(elem, doc):
@@ -15,18 +15,9 @@ def process_code_block(elem, doc):
     logging.info("Found Code Block")
     logging.info(elem)
     newel = RawBlock(elem.text, format="context")
-    if doc.format == 'context':
-        left = RawBlock('\start' + elem.classes[0], format="context")
-        right = RawBlock('\stop' + elem.classes[0], format="context")
-    else: 
+    if doc.format != 'context':
         return
-    if left:
-        newel.text = left.text + newel.text
-    else:
-        newel.text = newel.text
-    if right:
-        newel.text += right.text
-    return newel
+    return RawBlock(f'\\start{elem.classes[0]}\n{elem.text}\n\\stop{elem.classes[0]}', format="context")
 
 def main(doc=None):
     return run_filter(action, doc=doc)
