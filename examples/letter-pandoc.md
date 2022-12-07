da06ba16b821256ef6cb79eec9b7369e991001bc
Dennis Burgermeister <bug@pmhs.de>
2022-12-07 12:41:10 +0100

    (c) updates

---
+++ examples/letter-pandoc.md
@@ -0,0 +1,30 @@
+---
+author:
+- Dennis Burgermeister
+- PMHS Nürtingen
+opening: Lieber Max,
+closing: Herzliche Grüße,
+date: 12 December 2022
+address: 
+- PMHS Nürtingen
+return-address: 
+- PMHS Nürtingen
+- Kanalstraße 29
+- 72622 Nürtingen
+- "E-Mail: bug@pmhs.de"
+encl:
+- Kompetenzraster
+fontfamily: mathpazo
+fontsize: 12pt
+geometry: margin=1in
+blockquote: true
+closing-indentation: 0pt
+links-as-notes: true
+colorlinks: true
+...
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin mollis dolor vitae tristique eleifend. Quisque non ipsum sit amet velit malesuada consectetur. Praesent vel facilisis leo. Sed facilisis varius orci, ut aliquam lorem malesuada in. Morbi nec purus at nisi fringilla varius non ut dui. Pellentesque bibendum sapien velit. Nulla purus justo, congue eget enim a, elementum sollicitudin eros. Cras porta augue ligula, vel adipiscing odio ullamcorper eu. In tincidunt nisi sit amet tincidunt tincidunt. Maecenas elementum neque eget dolor [egestas fringilla](http://example.com):
+
+> Nullam eget dapibus quam, sit amet sagittis magna. Nam tincidunt, orci ac imperdiet ultricies, neque metus ultrices quam, id gravida augue lacus ac leo. 
+
+Vestibulum id sodales lectus, sed scelerisque quam. Nullam auctor mi et feugiat commodo. Duis interdum imperdiet nulla, vitae bibendum eros placerat non. Cras ornare, risus in faucibus malesuada, libero sem fringilla quam, ut luctus enim sapien eget dolor.
--- tools/csv_serienbrief_generator.py
+++ tools/csv_serienbrief_generator.py
@@ -1,6 +1,28 @@
 import csv
 import os
 import pandas as pd
+from string import Template
+import pandoc
+
+template = Template('''
+---
+author:
+- Dennis Burgermeister
+- PMHS Nürtingen
+closing: Herzliche Grüße,
+date: 12 December 2022
+return-address: 
+- "E-Mail: bug@pmhs.de"
+fontfamily: mathpazo
+fontsize: 12pt
+geometry: margin=1in
+blockquote: true
+closing-indentation: 0pt
+links-as-notes: true
+colorlinks: true
+...
+
+$content''')
 
 def read_csv(csv_path):
     return pd.read_csv(csv_path, encoding="ISO-8859-1", index_col=None, header=None)
@@ -11,14 +33,33 @@ def main(df):
     results = row_names[-5:]
     criteria = row_names[3:-7]
     for student in df:
+        if student == 0:
+            continue
         entry = list(df[student])
-        
-        print(f"Hallo {entry[0]} {entry[1]},")
-        print("Im Detail wurde deine Arbeit wie folgt bewertet:")
-        print("| Kriterium | Punkte |")
-        print("| --- | --- |")
+        text = f'''Liebe(r) {entry[0]} {entry[1]},
+
+Im Detail wurde deine Arbeit wie folgt bewertet:
+
+
+| Kriterium | Punkte |
+| --- | --- |
+'''
+
         for id, result in enumerate(entry[3:-7]):
-            print(f"| {criteria[id]} | {result} |")
+            text += f"| {criteria[id]} | {result} |\n"
+        text += f"""| Gesamtpunkte | {entry[-5]} |
+| von max. | {entry[-4]} |
+| **Notenpunkte** | **{entry[-3]}** |
+        
+Kommentare: 
+        
+{entry[-2]}
+
+Für Rückfragen stehe ich gerne zur Verfügung."""
+        text = template.substitute(content=text)
+        print(text)
+        doc = pandoc.read(text, format="markdown")
+        pandoc.write(doc, file=f"{entry[0]}_{entry[1]}.pdf", format="latex")
 
 if __name__ == "__main__":
-    main(read_csv("/mnt/d/zurcloud/Schule/2022_23/TGG12_INF/22_Publikationen/Projekt/Bewertungsraster.csv"))
+    main(read_csv("/mnt/d/c1o/Schule/2022_23/TGG12_INF/22_Publikationen/Projekt/Bewertungsraster.csv"))
