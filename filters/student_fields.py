bac129ab56b285fc8b6062b516154259f1503338
Dennis Burgermeister <dennis@zumlernen.de>
2023-05-16 16:12:24 +0200

    (+) Student fields: box+grid

--- filters/student_fields.py
+++ filters/student_fields.py
@@ -9,11 +9,21 @@ import logging
 
 
 def action(elem, doc):
+    """
+    []{.line}
+    []{lines=3}
+    []{grid=5 steps=0.5}
+    []{box=5}
+    """
     if type(elem) == Span:
         if "line" in elem.classes:
             return process_line(elem, doc)
         elif "lines" in elem.attributes:
             return process_lines(elem, doc)
+        elif "box" in elem.attributes:
+            return process_box(elem, doc)
+        elif "grid" in elem.attributes:
+            return process_grid(elem, doc)
 
 def process_line(elem, doc):
     logging.info("DrawLine")
@@ -26,6 +36,27 @@ def process_lines(elem, doc):
     lines = elem.attributes["lines"]
     if doc.format == "latex":
         return RawInline(f'~\\\\ \\drawLines{{{lines}}}\\\\~', format="latex")
+    return elem
+
+def process_box(elem, doc):
+    logging.info("Draw Box")
+    box = elem.attributes["box"]
+    if "grid" in elem.classes:
+        return RawInline(f'~\\\\ \\drawGridBox{{{box}}}\\\\~', format="latex")
+    if doc.format == "latex":
+        return RawInline(f'~\\\\ \\drawBox{{{box}}}\\\\~', format="latex")
+
+    return elem
+
+def process_grid(elem, doc):
+    logging.info("Draw Grid")
+    grid = elem.attributes["grid"]
+    if "steps" in elem.attributes:
+        steps = elem.attributes["steps"]
+        return RawInline(f'~\\\\ \\drawGrid[1.0][{steps}]{{{grid}}}\\\\~', format="latex")
+    if doc.format == "latex":
+        return RawInline(f'~\\\\ \\drawGrid[1.0][1.0]{{{grid}}}\\\\~', format="latex")
+
     return elem
 
 def main(doc=None):
