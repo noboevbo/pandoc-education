3c5d2b45e48634eaec9df7b0fc53fc34ce6d20b5
Dennis Burgermeister <dennis@zumlernen.de>
2024-01-11 10:49:35 +0100

    (+) Possibility to print expecations in exercise

--- filters/expectations.py
+++ filters/expectations.py
@@ -4,7 +4,7 @@
 Processes exercises
 """
 
-from panflute import Div, run_filter, Str, Span, Table, TableBody, TableHead, TableCell, TableRow, Caption, Para, convert_text, TableFoot, RawInline
+from panflute import Div, run_filter, Str, Span, Table, TableBody, TableHead, TableCell, TableRow, Caption, Para, convert_text, TableFoot, RawInline, RawBlock, stringify
 from utils.helpers import get_exercise_for_element
 from utils.models import Expectation, ExerciseExpectation
 import logging
@@ -22,7 +22,10 @@ def process_expectation(elem, doc):
         return
     points = elem.attributes["points"] if "points" in elem.attributes else 0
     bonus_points = elem.attributes["bonus-points"] if "bonus-points" in elem.attributes else 0
-    doc.exercise_expectations[-1].expectations.append(Expectation(elem.content, float(points),  float(bonus_points)))
+
+    expectations = Expectation(elem.content, float(points),  float(bonus_points))
+
+    doc.exercise_expectations[-1].expectations.append(expectations)
 
 def add_points_to_exercise(elem, doc, points, bonuspoints):
     elem.attributes["points"] = str(points)
@@ -40,9 +43,28 @@ def process_expectations(elem, doc):
     # TODO: Update exercise with points!
     doc.exercise_expectations.append(ExerciseExpectation(exercise, []))
     elem.walk(process_expectation, doc)
+
+    table = f"\\begin{{table}}[htbp]\\centering\\begin{{tabularx}}{{\\textwidth}}{{| X | c |}}\\hline\n"
+    rows = []
+    for expectation in doc.exercise_expectations[-1].expectations:
+        bonus = "" if expectation.bonuspoints == 0 else f" (+{expectation.bonuspoints})"
+        table += stringify(expectation.content)
+        table += " & "
+        table += f"{expectation.points}{bonus}"
+        table += "\\\\ \\hline\n"
+    table += f"\\end{{tabularx}}\\end{{table}}"
+    if "printhere" in elem.classes:
+        return RawBlock(table, format="latex")
     return []
 
-def write_expectations_table(exercise_expectations, doc):
+def get_expectation_row(expectation):
+    bonus = "" if expectation.bonuspoints == 0 else f" (+{expectation.bonuspoints})"
+    cell1 = TableCell(Para(*expectation.content))
+    cell2 = TableCell(*convert_text((f"{expectation.points}{bonus}")))
+    cell3 = TableCell()
+    return TableRow(cell1, cell2, cell3)
+
+def get_expectations_table(exercise_expectations, doc):
     header1 = TableCell(*convert_text('Die Schülerin/der Schüler'))
     header2 = TableCell(*convert_text('max. Punkte'))
     header3 = TableCell(*convert_text('erreicht'))
@@ -60,11 +82,7 @@ def write_expectations_table(exercise_expectations, doc):
             bonuspoints += expectation.bonuspoints
             exercise_points += expectation.points
             exercise_bonuspoints += expectation.bonuspoints
-            bonus = "" if expectation.bonuspoints == 0 else f" (+{expectation.bonuspoints})"
-            cell1 = TableCell(Para(*expectation.content))
-            cell2 = TableCell(*convert_text((f"{expectation.points}{bonus}")))
-            cell3 = TableCell()
-            rows.append(TableRow(cell1, cell2, cell3))
+            rows.append(get_expectation_row(expectation))
         add_points_to_exercise(expectations.exercise.elem, doc, exercise_points, exercise_bonuspoints)
     footer1 = TableCell(*convert_text('**Gesamt**'))
     footer2 = TableCell(*convert_text(f"{points} (+{bonuspoints})"))
@@ -73,7 +91,10 @@ def write_expectations_table(exercise_expectations, doc):
     footer = TableFoot(footer_row)
     body = TableBody(*rows)
     caption = Caption(Para(Str('Erwartungshorizont')))
-    table = Table(body, head=header, foot=footer, caption=caption, colspec=[('AlignDefault', 0.7), ('AlignDefault', 0.18), ('AlignDefault', 0.12)])
+    return Table(body, head=header, foot=footer, caption=caption, colspec=[('AlignDefault', 0.7), ('AlignDefault', 0.18), ('AlignDefault', 0.12)])
+
+def write_expectations_table(exercise_expectations, doc):
+    table = get_expectations_table(exercise_expectations, doc)
     doc.replace_keyword('$expectations', table)
 
 def finalize(doc):
